babaf23a070547435b38e8707274b92988869650
jashimgit <jashim.ncn@gmail.com>
2021-02-14 18:08:34 +0600

    remove an item from selected image done

--- app.js
+++ app.js
@@ -48,15 +48,20 @@ const getImages = (query) => {
 let slideIndex = 0;
 const selectItem = (event, img) => {
   let element = event.target;
+
   element.classList.add('added');
 
   let item = sliders.indexOf(img);
+
   if (item === -1) {
     sliders.push(img);
   } else {
-    alert('Hey, Already added !')
+    element.classList.toggle('added');
+    // alert('Hey, Already added !')
+    sliders.pop(img);
   }
 }
+
 var timer
 const createSlider = () => {
   // check slider image length
